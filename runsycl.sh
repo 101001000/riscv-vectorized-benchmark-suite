f58c38fba13bba835e3c51dc06b565f1ebdd4de5
Enrique de la Calle <encalle@ucm.es>
2025-03-25 11:31:47 +0100

    sycl run all ttest

---
+++ runsycl.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+CURRENT_DIR=$(pwd)
+
+# Lista de aplicaciones
+APPS=(
+  "axpy"
+  "blackscholes"
+  "canneal"
+  "jacobi-2d"
+  "lavaMD"
+  "matmul"
+  "spmv"
+  "swaptions"
+  "streamcluster"
+  "somier"
+  "particlefilter"
+  "pathfinder"
+)
+
+SYCLAPPS=(
+  "axpy"
+  "matmul"
+)
+
+echo "-------------------------------------------------------------------"
+echo "RiVec Benchmark Suite - Full Run"
+echo "(Ejecuta cada benchmark en modo 'vector' y 'serial' con 'large')"
+echo "-------------------------------------------------------------------"
+echo ""
+
+
+for app in "${SYCLAPPS[@]}"; do
+    echo ">>> Aplicación: $app"
+    cd "_${app}" || { echo "No se encuentra _$app"; continue; }
+
+    echo "   - Ejecutando en modo vector..."
+    echo -e "nosim\nvector\nlarge" | ./run.sh
+
+    echo "   - Ejecutando en modo serial..."
+    echo -e "nosim\nserial\nlarge" | ./run.sh
+
+    echo "   - Ejecutando en modo auto..."
+    echo -e "nosim\nauto\nlarge" | ./run.sh
+
+    echo "   - Ejecutando en modo sycl-serial..."
+    echo -e "nosim\nsycl-serial\nlarge" | ./run.sh
+
+    echo "   - Ejecutando en modo sycl-auto..."
+    echo -e "nosim\nsycl-auto\nlarge" | ./run.sh
+
+    cd "$CURRENT_DIR" || exit
+    echo ""
+done
